94d7a0afb04fdccbbb6c8fec609e35b5b7d92be1
meba-tesfaye <fiorella.mio1994@gmail.com>
2023-07-13 07:49:58 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#includ<stdio.h>
+/**
+* main - Entry point
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+        puts("\"programming is like bulding a multilingual puzzle");
+ return (0);
+}
