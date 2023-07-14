0376889b25eb47f0c4dea37a8c794b5e896944ee
meba-tesfaye <fiorella.mio1994@gmail.com>
2023-07-14 12:51:24 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,24 +8,20 @@ int main(void)
 {
 int n;
 int m;
-for (n = 0; n < 100; n++)
+for (n = 0; n <= 99; n++)
+for (m = n + 1; m <= 99; m++)
 {
-for (m = 0; m <= 99; m++)
-{
-if (n < m)
-{
-putchar(n / 10 + 48);
-putchar((n % 10) + 48);
+putchar(n / 10 + '0');
+putchar((n % 10) + '0');
 putchar(' ');
-putchar(n / 10 + 48);
-putchar((m % 10) + 48);
+putchar(m / 10 + '0');
+putchar((m % 10) + '0');
 if (n == 98 && m == 99)
 {
+continue;
+}
 putchar(',');
 putchar(' ');
-}
-}
-}
 }
 putchar('\n');
 return (0);
