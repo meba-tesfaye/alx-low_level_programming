fa4582cc2d71d6f8b9ff279925ba5eb1998d62fb
meba-tesfaye <fiorella.mio1994@gmail.com>
2023-07-18 04:32:34 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - make the alpahbet x10
+*
+* Return:void
+*/
+void print_alphabet_x10(void)
+{
+char m;
+int t;
+while (t <= 9)
+{
+t--;
+}
+for (m = 'a'; m <= 'z'; m++)
+{
+_putchar(m);
+}
+_putchar('\n');
+}
