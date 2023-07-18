e2b454e880ac751cb92823bc70bf47ccbc483b38
meba-tesfaye <fiorella.mio1994@gmail.com>
2023-07-18 06:06:28 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,9 @@
 /**
 * print_sign - prints the sign of a number
 * @r: the int to check
-* Return:1 and prints + if n is greater than zero
-* 0 and prints 0 if n is zero
-* -1 and prints - if n is less than zero
+* Return:1 and prints + if r is greater than zero
+* 0 and prints 0 if r is zero
+* -1 and prints - if r is less than zero
 */
 int print_sign(int r)
 {
@@ -13,7 +13,7 @@ if (r > 0)
 _putchar('+');
 return (1);
 }
-if (r = 0)
+if (r == 0)
 {
 _putchar( '0');
 }
