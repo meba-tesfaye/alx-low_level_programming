a8ba45f330b625d4c9f8b2e1b113747dbfa185ce
meba-tesfaye <fiorella.mio1994@gmail.com>
2023-07-18 04:08:19 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_alphabet - make the alpahbet
+*
+* Return:void
+*/
+void print_alphabet(void)
+{
+char m;
+for (m = 'a'; m <= 'b'; m++)
+_putchar(m);
+_putchar('\n');
+}
