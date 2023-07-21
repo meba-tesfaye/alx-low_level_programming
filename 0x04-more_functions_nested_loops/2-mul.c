ad508ebb161e9d780f293da79d37fd380407c84a
meba-tesfaye <fiorella.mio1994@gmail.com>
2023-07-21 01:22:04 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _isdigit - check if a letter is upper
+* @a: The first number to multiply
+* @b: The second number to multiply with first number
+* Return: The result of multiplication between a param and b param
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
