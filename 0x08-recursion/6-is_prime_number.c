51c107735d6ae31c91104863b0dced9448e7ecb8
meba-tesfaye <fiorella.mio1994@gmail.com>
2023-08-02 05:12:28 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,7 +8,7 @@ int check_prime(int n, int i);
 */
 int is_prime_number(int n)
 {
-return (check_prime(n, 1));
+return (check_prime(n, 0));
 }
 /**
 * check_prime - check if number is prime
@@ -19,10 +19,10 @@ return (check_prime(n, 1));
 int check_prime(int n, int i)
 {
 if (n <= 1)
-return (1);
-if (n % i == 0 && i > 1)
-return (1);
-if ((n / 1) < 1)
 return (0);
+if (n % i == 0 && i > 1)
+return (0);
+if ((n / 1) < 1)
+return (1);
 return (check_prime(n, i + 1));
 }
