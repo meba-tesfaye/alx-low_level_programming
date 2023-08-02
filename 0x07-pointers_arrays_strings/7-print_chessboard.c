fc6de83182b57d553fa307ca53fadfcf80c3e8e8
meba-tesfaye <fiorella.mio1994@gmail.com>
2023-08-02 05:37:09 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,13 +1,13 @@
-#include "main.h" 
+#include "main.h"
 /**
-* print_chessboard - entry point 
+* print_chessboard - entry point
 * @a: array
 * Return: void
 */
 void print_chessboard(char (*a)[8])
 {
 int i, j;
- for (i = 0; i < 8; i++)
+for (i = 0; i < 8; i++)
 {
 for (j = 0; j < 8; j++)
 {
